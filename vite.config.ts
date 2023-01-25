d568384beaccc7b42666db8e39fab0e0291a3769
unknown <kolahola149@gmail.com>
2023-01-25 12:13:10 +0300

    build as single file

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
+import { viteSingleFile } from 'vite-plugin-singlefile';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), vanillaExtractPlugin(), VitePWA()],
+  plugins: [tsconfigPaths(), react(), vanillaExtractPlugin(), VitePWA(), viteSingleFile()],
   build: {
     target: 'es2015',
   },
