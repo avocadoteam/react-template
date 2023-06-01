00d1a105fcd9fed5e9f06bb85697ff98fa44aa07
unknown <kolahola149@gmail.com>
2023-06-01 16:36:25 +0300

    remove less import

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useAppInit, useUI } from '@core/hooks';
 import { PromoBanner, useNativeBanner } from '@ui/ads';
-import { AppLayout } from '@ui/app-layout/AppLayout';
+import { AppLayout } from '@ui/app-layout';
 import { Snackbars } from '@ui/snackbars';
 import '@ui/theme/fonts.css';
 import { AdaptivityProvider, ConfigProvider } from '@vkontakte/vkui';
